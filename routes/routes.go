1633dff8a7a9f8c577aa321f9d1baf8cadca9c6f
FaaizHaikal <faaizhaikal@gmail.com>
2025-04-18 23:07:28 +0700

    feat: add routes

---
+++ routes/routes.go
@@ -0,0 +1,13 @@
+package routes
+
+import (
+	"github.com/FaaizHaikal/spendiary-backend/controllers"
+	"github.com/gofiber/fiber/v2"
+)
+
+func Initialize(app *fiber.App) {
+	api := app.Group("/api")
+
+	api.Post("/register", controllers.Register)
+	api.Post("/login", controllers.Login)
+}
