66b0e4728e45ca12ae0fece6027079a75f906367
FaaizHaikal <faaizhaikal@gmail.com>
2025-04-18 23:25:45 +0700

    refactor: add on delete cascade

--- models/user.go
+++ models/user.go
@@ -4,8 +4,8 @@ import "gorm.io/gorm"
 
 type User struct {
 	gorm.Model
-	Username string `gorm:"size:50;not null;unique"`
-	Password string `gorm:"size:255;not null"`
-	Expenses []Expense
-	Savings  []Saving
+	Username string    `gorm:"size:50;not null;unique"`
+	Password string    `gorm:"size:255;not null"`
+	Expenses []Expense `gorm:"constraint:onDelete:CASCADE"`
+	Savings  []Saving  `gorm:"constraint:onDelete:CASCADE"`
 }
