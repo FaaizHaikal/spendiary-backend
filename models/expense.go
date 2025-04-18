a79119b9b80565756c8fd11dc7deeb010981cf7e
FaaizHaikal <faaizhaikal@gmail.com>
2025-04-18 22:50:26 +0700

    feat: create expense model

---
+++ models/expense.go
@@ -0,0 +1,17 @@
+package models
+
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
+
+type Expense struct {
+	gorm.Model
+	Date        time.Time `gorm:"not null"`
+	Description string    `gorm:"not null"`
+	Amount      float64   `gorm:"not null"`
+
+	UserID uint `gorm:"not null"`
+	User   User `gorm:"foreignKey:UserID"`
+}
