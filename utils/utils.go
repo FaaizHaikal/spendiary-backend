4b30cc20eced37b2f8b70525aabcf754c3935dce
FaaizHaikal <faaizhaikal@gmail.com>
2025-04-19 12:53:28 +0700

    refactor: ignore error on hash password

--- utils/utils.go
+++ utils/utils.go
@@ -2,10 +2,10 @@ package utils
 
 import "golang.org/x/crypto/bcrypt"
 
-func HashPassword(password string) (string, error) {
-	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
+func HashPassword(password string) string {
+	bytes, _ := bcrypt.GenerateFromPassword([]byte(password), 14)
 
-	return string(bytes), err
+	return string(bytes)
 }
 
 func CheckPasswordHash(password, hash string) bool {
